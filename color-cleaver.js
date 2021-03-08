c863b43315980b9ee9c6686116a845c32b715ec2
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-03-07 22:35:45 -0600

    [Add all changes]

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,28 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+
+if (color1 === undefined && color2 === undefined) {
+    console.log('No colors provided.');
+}
+
+else if (color1 !== undefined && color2 === undefined) {
+    const isColor1Valid = isValidSecondary(color1);
+    if (isColor1Valid) {
+        const deconstruction = colorDeconstructor(color1);
+        console.log('Deconstruction is', deconstruction);
+    } else {
+        console.log('Color 1 is not a valid secondary color');
+    }
+}
+
+else {
+    const isColor1Valid = isValidPrimary(color1);
+    const isColor2Valid = isValidPrimary(color2);
+    if (isColor1Valid && isColor2Valid) {
+        const combination = colorCombinator(color1, color2);
+        console.log('Combination is', combination);
+    } else {
+        console.log('At least one color is not a valid primary color.');
+    }
+}
